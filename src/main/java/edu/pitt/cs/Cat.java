b0e81c86a379c12991697f96f94443c437470100
园田海未 <18519644256@163.com>
2025-09-19 16:34:55 -0400

    1st version

--- src/main/java/edu/pitt/cs/Cat.java
+++ src/main/java/edu/pitt/cs/Cat.java
@@ -15,7 +15,7 @@ public interface Cat {
 			case MOCK:
 			    // TODO: Return a mock object that emulates the behavior of the real object, if you feel you need one.
 //				mock a cat class
-				return null;
+				return Mockito.mock(Cat.class);
 			default:
 				assert(false);
 				return null;
